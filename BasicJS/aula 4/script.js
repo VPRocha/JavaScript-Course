d09fe993513dac01a9e50b844d812c750ac11838
Vanessa Rocha <57269936+VPRocha@users.noreply.github.com>
2022-01-09 19:55:30 -0400

    Delete BasicJS directory

--- BasicJS/aula 4/script.js
+++
@@ -1,5 +0,0 @@
-const num1 = parseFloat(prompt("digite o primeiro numero")); // usando parseFloat para converter o valor digitado pelo usuario de string para number
-const num2 = parseFloat(prompt("digite o segundo numero"));
-const soma = num1 + num2; // variável de resultado
-
-console.log(`o resultado da soma de num1 + num2 é: ${soma}`);
--- BasicJS/aula 9/script.js
+++
@@ -1,8 +0,0 @@
-// conversão number para string
-let num1 = 123
-let num2 = 456
-
-console.log(num1 + num2)
-console.log(num1.toString() + num2) // o toString() não altera o valor da variável num1
-// num1 = num1.toString() // agora sim o valor do num1 foi alterado para string
-// console.log(typeOf(num1))
